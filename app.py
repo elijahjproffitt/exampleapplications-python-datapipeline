340c487b732dc175832df3836a9c3c231cf531b8
elijahjproffitt <84947728+elijahjproffitt@users.noreply.github.com>
2022-12-17 13:22:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import db_actions
 
 def main():
     """
-    - Gets s&p500 tickers, writes to company relation
-    - Gets s&p500 ticker history, writes to values relation (in progress)
+    - Gets s&p500 symbols, writes to company relation
+    - Gets s&p500 symbol history, writes to values relation (in progress)
 
     Parameters
     ----------
@@ -14,13 +14,13 @@ def main():
     -------
 
     """
-    # get symbols and basic company data data from wikipedia
+    # get symbols and basic company data data from web source (wikipedia)
     symbols, data = api_actions.get_company()
 
-    # write symbols to db
+    # write symbols to company table in postgres
     db_actions.write_company(data)
 
-    # get values with tickers 
+    # get values with symbols 
     api_actions.get_values(symbols)
 
 main()
